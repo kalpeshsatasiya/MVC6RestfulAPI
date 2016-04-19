296c8a0e1475358f8ca9f21ef841dcb769833c37
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-04-19 22:33:12 +0530

    Initial commit

---
+++ RESTfulAPI/src/RESTfulAPI/Controllers/StudentsController.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using RESTfulAPI.Models;
+
+namespace RESTfulAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Students")]
+    public class StudentsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public IEnumerable<Student> GetStudent()
+        {
+            return _context.Student;
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}", Name = "GetStudent")]
+        public async Task<IActionResult> GetStudent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            Student student = await _context.Student.SingleAsync(m => m.StudentId == id);
+
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Students/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent([FromRoute] int id, [FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            if (id != student.StudentId)
+            {
+                return HttpBadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new HttpStatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // POST: api/Students
+        [HttpPost]
+        public async Task<IActionResult> PostStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            _context.Student.Add(student);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (StudentExists(student.StudentId))
+                {
+                    return new HttpStatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetStudent", new { id = student.StudentId }, student);
+        }
+
+        // DELETE: api/Students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            Student student = await _context.Student.SingleAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Count(e => e.StudentId == id) > 0;
+        }
+    }
+}
